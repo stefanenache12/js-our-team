4e8bf12a9846c9eecf47c38a118339245f1c1bc4
stefanenache12 <stefanenache12@gmail.com>
2023-06-26 16:41:00 +0200

    END PROJECT+BONUS 2

--- js/main.js
+++ js/main.js
@@ -36,26 +36,45 @@ const ourTeam = [
     },
 ];
 
-let container = document.querySelector('.row');
+const container = document.querySelector('.row');
 
 for (let index = 0; index < ourTeam.length; index++) {
         const element = ourTeam[index];
 
-        
         let card = document.createElement('div');
-        card.classList.add('col')
+        card.classList.add('flip-card','col-5');
         container.appendChild(card);
 
+        let flipCardInner = document.createElement('div');
+        flipCardInner.classList.add('flip-card-inner');
+        card.appendChild(flipCardInner);
 
+        let flipCardFront = document.createElement('div');
+        flipCardFront.classList.add('flip-card-front');
+        flipCardInner.appendChild(flipCardFront);
+
+        let flipCardBack = document.createElement('div');
+        flipCardBack.classList.add('flip-card-back');
+        flipCardInner.appendChild(flipCardBack);
+        
         for (const key in element) {
 
             if (key === 'profileImg') {
-              let img = document.createElement('img');
-              img.src = element[key];
-              card.appendChild(img);
-            } else {
-              let cardInfo = document.createElement('h4');
-              cardInfo.innerHTML = `${key}: ${element[key]}`;
-              card.appendChild(cardInfo);
-        }}
+                
+                let img = document.createElement('img');
+                img.src = element[key];
+                flipCardFront.appendChild(img);
+            } 
+            else if (key === 'name'){
+
+                let nameContainer = document.createElement('h2');
+                nameContainer.innerHTML = `${element[key]}`;
+                flipCardBack.appendChild(nameContainer);
+            }
+            else {
+                let roleContainer = document.createElement('p');
+                roleContainer.innerHTML = `${element[key]}`;
+                flipCardBack.appendChild(roleContainer);
+            }
+        }
 };
