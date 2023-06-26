35269e2f25940590eb56b337ae8900d29c734912
stefanenache12 <stefanenache12@gmail.com>
2023-06-26 15:24:27 +0200

    END MILESTONE 1

---
+++ js/main.js
@@ -0,0 +1,45 @@
+const ourTeam = [
+    {
+        name: 'Wayne Barnett',
+        role: 'Founder & CEO',
+        profileImg: 'img/wayne-barnett-founder-ceo.jpg',
+    },
+
+    {
+        name: 'Angela Caroll',
+        role: 'Chief Editor',
+        profileImg: 'img/angela-caroll-chief-editor.jpg',
+    },
+
+    {
+        name: 'Walter Gordon',
+        role: 'Office Manager',
+        profileImg: 'img/walter-gordon-office-manager.jpg',
+    },
+
+    {
+        name: 'Angela Lopez',
+        role: 'Social Media Manager',
+        profileImg: 'img/angela-lopez-social-media-manager.jpg',
+    },
+
+    {
+        name: 'Scott Estrada',
+        role: 'Developer',
+        profileImg: 'img/scott-estrada-developer.jpg',
+    },
+
+    {
+        name: 'Barbara Ramos',
+        role: 'Graphic Designer',
+        profileImg: 'img/barbara-ramos-graphic-designer.jpg',
+    },
+];
+
+for (let index = 0; index < ourTeam.length; index++) {
+        const element = ourTeam[index];
+
+    for (const key in element) {
+        console.log(key,element[key])
+    };
+};
